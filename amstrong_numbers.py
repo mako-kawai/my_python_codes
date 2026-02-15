fabe9097d9d9260f3fd8d70387eaf23a657d05dc
mako <1728589097@qq.com>
2026-02-15 20:09:34 +0800

    Delete amstrong_numbers.py

--- amstrong_numbers.py
+++
@@ -1,7 +0,0 @@
-for n in range(100,1000):
-    if n==sum([int(i)**3 for i in str(n)]):
-        print(n,end='\t')
-# This code finds and prints all Armstrong numbers (also known as narcissistic numbers) between 100 and 999.
-# An Armstrong number is a three-digit number that is equal to the sum of the cubes of its digits.
-# For example, 153 is an Armstrong number because 1^3 + 5^3 + 3^3 = 153.
-# The output will be the Armstrong numbers separated by tabs.
